11d50a350d78fb8b1a7adf520732a0feae7bb482
unknown <jacovancr@gmail.com>
2021-03-06 16:21:30 +0000

    Initial structure

---
+++ src/views/Portfolio.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="portfolio">
+    <WebsiteDisplay/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import WebsiteDisplay from "@/components/HelloWorld.vue";
+
+export default {
+  name: "Home",
+  components: {
+    WebsiteDisplay
+  }
+};
+</script>
