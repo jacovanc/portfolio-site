01f2d43d6024a0cec66b219f212745e65dc67f7f
unknown <jacovancr@gmail.com>
2021-03-07 18:04:46 +0000

    Mobile slideable pages

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,96 @@
 <template>
-	<div>
+	<div class="main-wrapper">
 		<div id="nav">
-			<router-link to="/" class="site-title">
-				Jaco van Cranenburgh
-			</router-link>
-			<div class="links">
-				<router-link to="/">About</router-link> |
-				<router-link to="/portfolio">Portfolio</router-link> |
-				<router-link to="/blog">Blog</router-link>
+			<div class="inner-wrapper">
+				<router-link to="/" class="site-title">
+					Jaco van Cranenburgh
+				</router-link>
+				<div class="links">
+					<router-link to="/">About</router-link> |
+					<router-link to="/portfolio">Portfolio</router-link> |
+					<router-link to="/blog">Blog</router-link>
+				</div>
+				<div class="clear"></div>
 			</div>
-			<div class="clear"></div>
 		</div>
-		<div class="outer-wrapper">
+		<div class="outer-wrapper" id="touchsurface" v-on:touchstart="touchStart" v-on:touchend="touchEnd">
 			<router-view />
 		</div>
 	</div>
 </template>
 
+<script>
+export default {
+	data() {
+		return {
+			swipedir: null,
+			startX: null,
+			startY: null,
+			distX: null,
+			distY: null,
+			threshold: 150,
+			restraint: 100,
+			allowedTime: 300,
+			elapsedTime: null,
+			startTime: null,
+
+			pages: ["/", "/portfolio", "/blog"],
+		}
+	},
+	computed: {
+		currentRoute() {
+			return this.$route.path;
+		},
+		pageIndex() {
+			return this.pages.indexOf(this.currentRoute);
+		}
+	},
+	methods: {
+		// Handle swiping listener for route changes on mobile
+		touchStart: function(e){
+			var touchobj = e.changedTouches[0]
+			this.swipedir = 'none'
+			this.startX = touchobj.pageX
+			this.startY = touchobj.pageY
+			this.startTime = new Date().getTime() // record time when finger first makes contact with surface
+			// e.preventDefault()
+		},	
+		touchEnd: function(e){
+			var touchobj = e.changedTouches[0]
+			this.distX = touchobj.pageX - this.startX // get horizontal dist traveled by finger while in contact with surface
+			this.distY = touchobj.pageY - this.startY // get vertical dist traveled by finger while in contact with surface
+			this.elapsedTime = new Date().getTime() - this.startTime // get time elapsed
+			if (this.elapsedTime <= this.allowedTime){ // first condition for awipe met
+				if (Math.abs(this.distX) >= this.threshold && Math.abs(this.distY) <= this.restraint){ // 2nd condition for horizontal swipe met
+					this.swipedir = (this.distX < 0)? 'left' : 'right' // if dist traveled is negative, it indicates left swipe
+				}
+				else if (Math.abs(this.distY) >= this.threshold && Math.abs(this.distX) <= this.restraint){ // 2nd condition for vertical swipe met
+					this.swipedir = (this.distY < 0)? 'up' : 'down' // if dist traveled is negative, it indicates up swipe
+				}
+			}
+			if(this.pageIndex >= 0 && this.pageIndex <= this.pages.length-1) {
+				if(this.swipedir == "right") {
+					this.nextRoute = this.pages[this.pageIndex - 1];
+				} else if (this.swipedir == "left") {
+					this.nextRoute = this.pages[this.pageIndex + 1];
+				}
+				if(this.nextRoute !== undefined) {
+					this.$router.push(this.nextRoute);
+				}
+			}
+		}
+	}
+}
+</script>
+
+
 <style>
+	body {
+		height: 100vh;
+	}
+	#app, .main-wrapper {
+		height: 100%;
+	}
 	/* Generic */
 	.clear {
 		clear: both;
@@ -48,6 +121,7 @@
 	}
 	.outer-wrapper {
 		padding: 15px;
+		height: 100%;
 	}
 	.inner-wrapper {
 		width: 840px;
@@ -90,6 +164,7 @@
 	#nav .links a.router-link-exact-active {
 		color: #4457ff;
 	}
+
 	@media screen and (max-width: 530px) {
 		#nav {
 			text-align: left;
